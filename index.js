1ad2929c76b7e979ee52848e49e0664b8feff81f
Thomas Bulle <41519508+thomasbulle@users.noreply.github.com>
2022-07-29 15:44:10 +0200

    add the solution

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const toggleLight = () => {
+  const lightImg = document.getElementById('lightImg');
+  const toggleBtnLabel = document.getElementById('toggleBtnLabel');
+
+  if (lightImg.src.includes('off')) {
+    lightImg.src = lightImg.src.replace('off', 'on');
+    toggleBtnLabel.textContent = 'Turn off';
+  } else {
+    lightImg.src = lightImg.src.replace('on', 'off');
+    toggleBtnLabel.textContent = 'Turn on';
+  }
+}
+
 const init = () => {
-  // Your code here ...
+  const toggleBtn = document.getElementById('toggleBtn');
+  toggleBtn.addEventListener('click', toggleLight);
 }
 
 window.onload = init;
